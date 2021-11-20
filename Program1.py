edf38e1f03665012056b285a083c05d098f42b24
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-20 11:04:01 +0800

    added code for getting given 4 numbers

---
+++ Program1.py
@@ -0,0 +1,20 @@
+
+def enter_4_Numbers ():
+    a = int (input (f"Give 1st number: "))
+    b = int (input (f"Give 2nd number: "))
+    c = int (input (f"Give 3rd number: "))
+    d = int (input (f"Give 4th number: "))
+    return a, b, c, d
+
+
+
+
+
+
+a, b, c, d = enter_4_Numbers ()
+
+
+
+#Steps: 
+#Step 1:Create a program that ask for 4 numbers. 
+#Step 2:Print the 4 numbers from highest to lowest using only if-else statement.
