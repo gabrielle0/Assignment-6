5054ae085c3b44a9f3a4d35fc743ce73c42bd200
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-20 21:25:02 +0800

    added functions for first question, checking first user answer and displaying total score

--- Program2.py
+++ Program2.py
@@ -10,7 +10,25 @@ import random
 def item1 ():
     given1 = random.randint (0, 99)
     given2 = random.randint (0, 99) 
-    return answer1
+    answer1 = int (input (f"{given1} + {given2} = "))
+    correct_Answer1 = given1 + given2
+    return answer1, correct_Answer1
 
 
-answer1 = item1 ()
+def check_Item1 ():
+    if answer1 == correct_Answer1:
+        score1 = 1
+    else:
+        score1 = 0
+    return score1
+
+
+def get_Total_Score (score1):
+    total = score1 
+    print (f"Your score is: {total}/10. ")
+
+
+
+answer1, correct_Answer1 = item1 ()
+score1 = check_Item1 ()
+get_Total_Score (score1)
