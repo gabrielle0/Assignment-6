f10316293bb7288eae06787d38cebbefc2eaa775
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-20 21:51:50 +0800

    added functions for items 2,3 and 4 using similar function as item 1

--- Program2.py
+++ Program2.py
@@ -14,7 +14,6 @@ def item1 ():
     correct_Answer1 = given1 + given2
     return answer1, correct_Answer1
 
-
 def check_Item1 ():
     if answer1 == correct_Answer1:
         score1 = 1
@@ -22,13 +21,73 @@ def check_Item1 ():
         score1 = 0
     return score1
 
+def item2 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer2 = int (input (f"{given1} + {given2} = "))
+    correct_Answer2 = given1 + given2
+    return answer2, correct_Answer2
 
-def get_Total_Score (score1):
-    total = score1 
+def check_Item2 ():
+    if answer2 == correct_Answer2:
+        score2 = 1
+    else:
+        score2 = 0
+    return score2
+
+def item3 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer3 = int (input (f"{given1} + {given2} = "))
+    correct_Answer3 = given1 + given2
+    return answer3, correct_Answer3
+
+def check_Item3 ():
+    if answer3 == correct_Answer3:
+        score3 = 1
+    else:
+        score3 = 0
+    return score3
+
+def item4 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer4 = int (input (f"{given1} + {given2} = "))
+    correct_Answer4 = given1 + given2
+    return answer4, correct_Answer4
+
+def check_Item4 ():
+    if answer4 == correct_Answer4:
+        score4 = 1
+    else:
+        score4 = 0
+    return score4
+
+
+
+
+
+def get_Total_Score (score1, score2, score3, score4 ):
+    total = score1 + score2 + score3 + score4
     print (f"Your score is: {total}/10. ")
-
 
 
 answer1, correct_Answer1 = item1 ()
 score1 = check_Item1 ()
-get_Total_Score (score1)
+
+answer2, correct_Answer2 = item2 ()
+score2 = check_Item2 ()
+
+answer3, correct_Answer3 = item3 ()
+score3 = check_Item3 ()
+
+answer4, correct_Answer4 = item4 ()
+score4 = check_Item4 ()
+
+
+
+
+
+
+get_Total_Score (score1, score2, score3, score4)
+
