102cdbb087eb702e1b2e0785a2a2187b683ee849
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-20 22:08:08 +0800

    added code for items 6-10, edited code for displaying score

--- Program2.py
+++ Program2.py
@@ -4,7 +4,6 @@
 #Step 3: The program will ask the user 10 addition operations.
 #Step 4: Display the result summary of the 10 operations (ex 9/10).
 
-
 import random
 
 def item1 ():
@@ -63,12 +62,93 @@ def check_Item4 ():
         score4 = 0
     return score4
 
+def item5 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer5 = int (input (f"{given1} + {given2} = "))
+    correct_Answer5 = given1 + given2
+    return answer5, correct_Answer5
+
+def check_Item5 ():
+    if answer5 == correct_Answer5:
+        score5 = 1
+    else:
+        score5 = 0
+    return score5
+
+def item6 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer6 = int (input (f"{given1} + {given2} = "))
+    correct_Answer6 = given1 + given2
+    return answer6, correct_Answer6
+
+def check_Item6 ():
+    if answer6 == correct_Answer6:
+        score6 = 1
+    else:
+        score6 = 0
+    return score6
+
+def item7 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer7 = int (input (f"{given1} + {given2} = "))
+    correct_Answer7 = given1 + given2
+    return answer7, correct_Answer7
+
+def check_Item7 ():
+    if answer7 == correct_Answer7:
+        score7 = 1
+    else:
+        score7 = 0
+    return score7
+
+def item8 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer8 = int (input (f"{given1} + {given2} = "))
+    correct_Answer8 = given1 + given2
+    return answer8, correct_Answer8
+
+def check_Item8 ():
+    if answer8 == correct_Answer8:
+        score8 = 1
+    else:
+        score8 = 0
+    return score8
+
+def item9 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer9 = int (input (f"{given1} + {given2} = "))
+    correct_Answer9 = given1 + given2
+    return answer9, correct_Answer9
+
+def check_Item9 ():
+    if answer9 == correct_Answer9:
+        score9 = 1
+    else:
+        score9 = 0
+    return score9
+
+def item10 ():
+    given1 = random.randint (0, 99)
+    given2 = random.randint (0, 99) 
+    answer10 = int (input (f"{given1} + {given2} = "))
+    correct_Answer10 = given1 + given2
+    return answer10, correct_Answer10
+
+def check_Item10 ():
+    if answer10 == correct_Answer10:
+        score10 = 1
+    else:
+        score10 = 0
+    return score10
 
 
-
-
-def get_Total_Score (score1, score2, score3, score4 ):
-    total = score1 + score2 + score3 + score4
+def get_Total_Score (score1, score2, score3, score4, score5, score6, score7, score8, score9, score10 ):
+    total = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 + score9 + score10
     print (f"Your score is: {total}/10. ")
 
 
@@ -84,10 +164,23 @@ score3 = check_Item3 ()
 answer4, correct_Answer4 = item4 ()
 score4 = check_Item4 ()
 
+answer5, correct_Answer5 = item5 ()
+score5 = check_Item5 ()
 
+answer6, correct_Answer6 = item6 ()
+score6 = check_Item6 ()
 
+answer7, correct_Answer7 = item7 ()
+score7 = check_Item7 ()
 
+answer8, correct_Answer8 = item7 ()
+score8 = check_Item8 ()
 
+answer9, correct_Answer9 = item7 ()
+score9 = check_Item9 ()
 
-get_Total_Score (score1, score2, score3, score4)
+answer10, correct_Answer10 = item7 ()
+score10 = check_Item10 ()
+
+get_Total_Score (score1, score2, score3, score4, score5, score6, score7, score8, score9, score10)
 
